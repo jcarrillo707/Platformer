0c601265af67dfbdc4b0984e8c2b732a2b696c25
Pieslice24 <pieslice24@gmail.com>
2024-10-31 16:04:41 -0700

    One last little change to the movement

--- Metroid/Assets/Scrpits/PlayerController.cs
+++ Metroid/Assets/Scrpits/PlayerController.cs
@@ -29,7 +29,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
-            rb.velocity 
+            //rb.velocity 
 
         }
     }
