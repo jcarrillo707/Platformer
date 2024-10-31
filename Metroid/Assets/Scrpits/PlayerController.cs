40298336cac35a5c127d448f1c87bb70d5e5e3bd
Pieslice24 <pieslice24@gmail.com>
2024-10-31 14:48:16 -0700

    Testing movement

--- Metroid/Assets/Scrpits/PlayerController.cs
+++ Metroid/Assets/Scrpits/PlayerController.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public CharacterController controller;
 
-
-
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Debug.Log (Input.GetAxisRaw("Horizontal"));
+    }
 }
