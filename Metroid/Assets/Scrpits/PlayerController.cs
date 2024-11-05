19aed323c69313a5c4f17b582a9a0fd06f722943
Pieslice24 <pieslice24@gmail.com>
2024-11-05 15:33:18 -0800

    Added a material and finished the movement

--- Metroid/Assets/Scrpits/PlayerController.cs
+++ Metroid/Assets/Scrpits/PlayerController.cs
@@ -5,30 +5,56 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float playerSpeed = 10f;
+    public float playerSpeed = 3f;
 
 
     float speedDif;
-    float maxSpeed = 20f;
-
+    float maxSpeed = 5f;
+    float jumpForce = 5f;
 
     Rigidbody rb;
 
-    private void Update()
+    private void Start()
     {
-        speedDif = maxSpeed - rb.velocity.x;
+        rb = GetComponent<Rigidbody>();
+    }
 
+    private void FixedUpdate()
+    {
+        //speedDif = maxSpeed - rb.velocity.x;
+        //float movement = speedDif * playerSpeed;
+        Vector3 deltaMovement = Vector3.zero;
 
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(Vector3.left * speedDif);
 
+           // rb.AddForce(Vector3.left * playerSpeed, ForceMode.Force);
+            deltaMovement.x = -1;
 
         }
 
+        if (Input.GetKey(KeyCode.D))
+        {
 
+           // rb.AddForce(Vector3.right * playerSpeed, ForceMode.Force);
+            deltaMovement.x = 1;
+
+        }
+
+        rb.velocity = new Vector3(deltaMovement.x * playerSpeed, rb.velocity.y, rb.velocity.z); 
 
     }
 
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+        }
+
+    }
 
 }
